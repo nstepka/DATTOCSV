6e98b7d38051775f4aeec5ae7d7cc0439f15c4d1
Nicholas Ryan Stepka <nstepka@gmail.com>
2019-01-21 12:00:45 -0600

    updated files

---
+++ src/main/java/com/sg/fileconverter/DatToCSV.java
@@ -0,0 +1,52 @@
+package com.sg.fileconverter;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DatToCSV {
+    
+    public static final String DELIMITER = "#";
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = null;
+        List<String> datFileList = new ArrayList<>();
+        String FILE_NAME = "File.dat";
+       try {
+//            // Create Scanner for reading the file
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(FILE_NAME)));
+       } catch (IOException e) {
+			e.printStackTrace();
+		}
+        String datFileLine;
+
+        while (scanner.hasNextLine()) {
+            datFileLine = scanner.nextLine();
+            datFileList.add(datFileLine);
+        }
+        scanner.close();
+        convertDatToCsv("csvfile.csv", datFileList);
+    }
+
+    public static void convertDatToCsv(String FileName, List<String> datFile) throws IOException {
+        PrintWriter csvWriter = null;
+        try {
+            csvWriter = new PrintWriter(new FileWriter(FileName));
+        } catch (IOException ex) {
+            throw new IOException("Could not save dat file data", ex);
+        }
+        for (String lines : datFile) {
+            csvWriter.println(datFile);
+            csvWriter.flush();
+        }
+        csvWriter.close();
+    }
+}
